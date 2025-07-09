dc32a6ee762c18a5d31076e422671200957acac5
Kirakosjan <sargis.k20@tumo.org>
2025-07-09 21:23:09 +0400

    avidcard

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
-<script setup>
+<template>
+  <main class="main">
+    <h1>Note App</h1>
+    <Card />
+  </main>
 
+
+</template>
+<script>
+import Card from './Components/Card.vue' 
 </script>
 
-<template>
-  
-</template>
+<style>
+body {
+  width: 100%;
+  height: 100%;
+}
 
-<style scoped></style>
+main.main {
+  background: linear-gradient(128deg, rgba(135, 246, 180, 0.84) 12.75%, rgba(50, 180, 187, 0.67) 83.4%);
+
+  width: 100%;
+  min-height: 100vh;
+}
+
+h1 {
+
+  color: black;
+  text-align: center;
+  padding: 24px;
+}
+</style>
---
+++ src/Components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card">
+        <p class="title"> My Title</p>
+        <p class="counter">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the i...
+        </p>
+        <p class="tvakan">
+            05-07-2025 20:30
+        </p>
+    </div>
+</template>
+
+<style>
+.card {
+    width: 200px;
+    height: 100px;
+    border: 1px;
+    color: black;
+    background: linear-gradient(114.43deg, rgba(255, 255, 255, 0.6) 4%, rgba(255, 255, 255, 0.1) 100%);
+    border-radius: 14px;
+}
+</style>
+
+<script>
+</script>
